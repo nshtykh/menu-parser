d38220b36e8bea5244f011d8ab9491f4ed2e8fda
Nataliia Shtykh <natalia.shtyh9@gmail.com>
2024-03-29 15:30:48 +0200

    init commit

---
+++ src/main/java/org/example/menuparser/MenuParserApplication.java
@@ -0,0 +1,13 @@
+package org.example.menuparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuParserApplication.class, args);
+	}
+
+}
