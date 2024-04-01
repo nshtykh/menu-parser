e863049f06211e9588ebf163cd0eabea3d4a3a13
Nataliia Shtykh <natalia.shtyh9@gmail.com>
2024-04-01 09:59:29 +0300

    added report creator

--- src/main/java/org/example/menuparser/MenuParserApplication.java
+++ src/main/java/org/example/menuparser/MenuParserApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MenuParserApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MenuParserApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MenuParserApplication.class, args);
+    }
 }
---
+++ src/main/java/org/example/menuparser/service/ReportCreator.java
@@ -0,0 +1,5 @@
+package org.example.menuparser.service;
+
+public interface ReportCreator {
+    void createReport(String resource, String fileName);
+}
